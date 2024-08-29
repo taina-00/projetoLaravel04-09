9e8c4baa7e948d124e98901e2e9765afa8bf2511
Rafael <jordao.jardim@hotmail.com.br>
2024-08-28 21:05:18 -0300

    projeto salvando no banco

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function criar(Request $request) {
+        $user = new User;
+        $user->name = $request->name;
+        $user->cpf = $request->cpf;
+        $user->email = $request->email;
+
+        $user->save();
+    }
+}
+
